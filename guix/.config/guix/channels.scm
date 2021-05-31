e244d273094ad683c6987f9698998ebfac4a0c99
Lucas Gruss <lucas.gruss@gmail.com>
2021-05-31 15:37:53 +0200

    guix: fix channels

--- guix/.config/guix/channels.scm
+++ guix/.config/guix/channels.scm
@@ -6,9 +6,7 @@
           "33f86a4b48205c0dc19d7c036c85393f0766f806"
           (openpgp-fingerprint
            "736A C00E 1254 378B A982  7AF6 9DBE 8265 81B6 4490"))))
-       %default-channels)
-
-(cons* (channel
+       (channel
         (name 'nonguix)
         (url "https://gitlab.com/nonguix/nonguix")
         ;; Enable signature verification:
