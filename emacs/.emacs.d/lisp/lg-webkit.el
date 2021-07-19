1d3244330973d96a2a695400fca5f728b18f8f27
Lucas Gruss <lucas@debian>
2021-07-19 11:16:41 +0200

    emacs : add webkit configuration for testing

---
+++ emacs/.emacs.d/lisp/lg-webkit.el
@@ -0,0 +1,42 @@
+(use-package webkit
+  :demand t
+  :straight
+  '(webkit :host github :repo "akirakyle/emacs-webkit"
+	   :branch "main"
+	   :files (:defaults "*.js" "*.css" "*.so")
+	   :pre-build "make")
+  :config
+  (modify-frame-parameters nil '((inhibit-double-buffering . t)))
+  (defun webkit--adjust-size (frame)
+    (ignore frame)
+    (dolist (buffer webkit--buffers)
+      (when (buffer-live-p buffer)
+	(with-current-buffer buffer
+	  (let* ((windows (get-buffer-window-list buffer 'nomini t)))
+	    (if (not windows)
+		(webkit--hide webkit--id)
+	      (let* ((show-window (if (memq (selected-window) windows)
+				      (selected-window)
+				    (car windows)))
+		     (hide-windows (remq show-window windows))
+		     (show-frame (window-frame show-window)))
+		(webkit--move-to-x-or-pgtk-frame show-frame)
+		(pcase-let ((`(,left ,top ,right ,bottom)
+			     (window-inside-pixel-edges show-window)))
+		  (webkit--show webkit--id)
+		  (if (and (bound-and-true-p centaur-tabs-mode))
+		      (webkit--resize webkit--id left (+ top centaur-tabs-height)
+				      (- right left) (- bottom top))
+		    (webkit--resize webkit--id left top
+				    (- right left) (- bottom top)))
+		  (dolist (window hide-windows)
+		    (switch-to-prev-buffer window)))))))))))
+
+(use-package evil-collection-webkit
+  :config
+  :after webkit
+  (evil-collection-xwidget-setup))
+
+(use-package webkit-ace)
+
+(use-package evil-collection-webkit)
