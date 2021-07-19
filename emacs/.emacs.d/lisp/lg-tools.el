d79381a96610f9a1a09f8e2c6593b59962b9bfd7
Lucas Gruss <lucas@debian>
2021-07-19 10:54:39 +0200

    emacs : refactor and reorganize

--- emacs/.emacs.d/lisp/lg-tools.el
+++ emacs/.emacs.d/lisp/lg-tools.el
@@ -1,26 +1,8 @@
 ;;; lg-tools.el : tools for emacs, to do everything in emacs
 
-(use-package pdf-tools
-  :straight t
-  :mode ("\\.pdf\\'" . pdf-view-mode)
-  :magic ("%PDF" . pdf-view-mode)
-  :config
-  (setq pdf-view-midnight-colors '("#ffffff" . "#000000")))
+;; * Dired
 
-(use-package vterm
-  :straight t
-  :bind ("s-<return>" . 'vterm)
-  :init
-  (setq vterm-shell "bash")
-  (setq vterm-always-compile-module t)
-  (setq vterm-module-cmake-args "-DUSE_SYSTEM_VTERM=YES"))
-
-(use-package ibuffer)
-(use-package ibuffer-sidebar :straight t)
-
-;; DIRED
 (use-package dired
-  :defer t
   :hook (dired-mode . dired-hide-details-mode)
   :config
   (setq dired-listing-switches "-al --group-directories-first")
@@ -28,23 +10,49 @@
     "h" #'dired-up-directory
     "l" #'dired-find-file))
 
-(use-package wdired)
-
 (use-package diredfl
   :straight t
+  :after dired
   :hook (dired-mode . diredfl-mode))
 
 (use-package dired-sidebar
   :straight t
+  :after dired
+  :commands (dired-sidebar-find-file dired-sidebar-toggle)
   :general
-  (general-def :keymaps 'dired-mode-map :states 'normal
+  (my-leader-def :keymaps 'override
+    "td" #'dired-sidebar-toggle-sidebar)
+  (general-def :keymaps 'dired-sidebar-mode-map :states 'normal
     "h" #'dired-sidebar-up-directory
-    "l" #'dired-sidebar-find-file))
+    "l" #'dired-sidebar-find-file)
+  :hook
+  (dired-sidebar-mode . (lambda () (setq-local header-line-format "Dired-sidebar")))
+  :config
+  (setq dired-sidebar-width 30)
+  ;;(setq dired-sidebar-mode-line-format nil)
+  (setq dired-sidebar-no-delete-other-windows t))
+
+(use-package dired-hide-dotfiles
+  :after dired
+  :hook (dired-mode . dired-hide-dotfiles-mode)
+  :straight t)
+
+;; * Elfeed
 
 (use-package elfeed
   :straight t
+  :commands (elfeed elfeed-update)
   :config
   (use-package lg-elfeed)
+  (setq elfeed-feeds
+	'(("https://xkcd.com/atom.xml" comic)
+	  ("https://protesilaos.com/codelog.xml" code)
+	  ("https://blog.tecosaur.com/tmio/rss.xml" org)
+	  ("https://www.youtube.com/feeds/videos.xml?channel_id=UC0uTPqBCFIpZxlz_Lv1tk_g" protesilaos)
+	  ("https://www.youtube.com/feeds/videos.xml?playlist_id=PL0H7ONNEUnnt59niYAZ07dFTi99u2L2n_" ouvrez_les_guillements usul)
+	  ("https://www.youtube.com/feeds/videos.xml?channel_id=UCKH_iLhhkTyt8Dk4dmeCQ9w" science)
+	  ("https://www.youtube.com/feeds/videos.xml?playlist_id=PL43OynbWaTMLEbdAWr-DnAfveOonmhlT1" france_inter)
+	  "https://api.lemediatv.fr/rss.xml"))
   ;; :bind ((:map elfeed-search-mode-map
   ;;         ("v" . #'elfeed-view-mpv)
   ;;         ("a" . #'elfeed-listen-mpv))
@@ -53,33 +61,35 @@
   ;;         ("a" . #'elfeed-listen-mpv)))
   :general
   (my-leader-def
-   :prefix "SPC" :states 'normal
-   "o" '(:ignore t :which-key "Open")
-   "of" '(elfeed :which-key "Open elfeed")
-   "oF" '(elfeed-update :which-key "update elfeed")))
+    :keymaps 'override
+    "o" '(nil :which-key "Open\n")
+    "of" '(elfeed :which-key "Open elfeed")
+    "oF" '(elfeed-update :which-key "update elfeed")))
 
 (use-package elfeed-org
   :disabled t
-  :straight t
   :after elfeed
   :config
   (elfeed-org)
   (setq rmh-elfeed-org-files "~/org/elfeed.org"))
 
+;; * Emms
+
 (use-package emms
   :straight t
-  :ensure-system-package ((mp3info . mp3info)
+  :ensure-system-package (exiftool
+			  (mp3info . mp3info)
 			  (mpv . mpv)
 			  (mplayer . mplayer))
+  :defer 5
   :init
   (setq emms-source-file-default-directory "~/Audio/Musique/")
   (setq emms-streams-file "~/.config/doom/emms/streams.emms")
   (setq emms-playlist-buffer-name "*Music*")
   (setq emms-info-asynchronously t) ; update tags asynchronously)
+  (setq emms-info-functions '(emms-info-exiftool))
   (setq emms-source-file-directory-tree-function 'emms-source-file-directory-tree-find)
-  ;;emms-player-list '(emms-player-mpg321 emms-player-ogg123 emms-player-mplayer-playlist emms-player-mplayer emms-player-mpv emms-player-vlc emms-player-vlc-playlist)
   (setq emms-player-list '(emms-player-mplayer emms-player-mpv))
-					;(setq emms-mode-line-mode-line-function nil)
   (setq emms-player-mpv-parameters '("--quiet" "--really-quiet" "--no-audio-display"))
 
   (defun lg/find-music-directory ()
@@ -87,17 +97,11 @@
     ;;(find-file "~/Audio/Musique")
     (let ((default-directory "~/Audio/Musique"))
       (dired-sidebar-toggle-sidebar)))
+
   :general
-  ;; (general-define-key :states 'normal
-  ;;   :keymaps 'emms-playlist-mode-map
-  ;; 	  "q" #'emms-playlist-mode-bury-buffer)
-  ;; (my-local-leader-def
-  ;;   :keymaps 'dired-mode-map
-  ;;   "m" 'emms-play-dired
-  ;;   "M" 'emms-add-dired)
   (my-leader-def
-    :prefix "SPC" :states 'normal :keymaps 'override
-    "e" '(:ignore t :which-key "emms")
+    :keymaps 'override
+    "e" '(nil :which-key "emms\n")
     "ej" #'emms-next
     "ek" #'emms-previous
     "es" #'emms-stop
@@ -105,31 +109,180 @@
     "eS" #'emms-shuffle
     "ea" #'emms-show-all
     "eb" #'lg/find-music-directory
-    "em" #'emms
+    "em" #'lg/emms-go-playlist
     "er" #'emms-streams)
   (my-local-leader-def
-    :states 'normal :keymaps '(dired-mode-map override)
+    :keymaps 'dired-sidebar-mode-map
     "" nil
     "m" #'emms-play-dired)
+  (general-def :states 'normal :keymaps 'emms-playlist-mode-map
+    "q" #'emms-playlist-mode-bury-buffer)
+
   :config
   (emms-all)
+
   (defun lg/emms-kill-mpv ()
     (interactive)
     (shell-command "killall mpv"))
+
+  (defcustom emms-sidebar-no-delete-other-windows t
+    "Whether or not to add no-delete-other-window parameter to window.")
+
+  (defcustom lg/emms-sidebar-display-alist '((side . left)
+					     (slot . -2))
+    "Parameters for the display of the emms-sidebar.")
+
+  (defun lg/emms-go-playlist ()
+    "Go to the playlist buffer. This function exist for the sake of
+the display-buffer-alist variable and enable the display of the
+playlist in a side-window"
+    (interactive)
+    (display-buffer-in-side-window (get-buffer-create "*Music*")
+				   lg/emms-sidebar-display-alist)
+    (let ((window (get-buffer-window (get-buffer "*Music*"))))
+      (when emms-sidebar-no-delete-other-windows
+	(set-window-parameter window 'no-delete-other-windows t))
+      (set-window-dedicated-p window t)
+      (with-selected-window window
+	(let ((window-size-fixed))
+	  (dired-sidebar-set-width dired-sidebar-width)))))
+
   (setq emms-info-functions '(emms-info-mp3info emms-info-cueinfo))
   (add-hook 'emms-player-stopped-hook #'lg/emms-kill-mpv))
 
-(use-package rg :commands rg :straight t
-  :ensure-system-package rg
+
+(use-package smudge
+  :ensure-system-package curl
+  :straight (smudge :host github :repo "danielfm/smudge")
+  :init
+  (setq httpd-port 8081)
+  (setq smudge-oauth2-callback-port "8081")
   :config
-  (setq rg-executable "/usr/bin/rg"))
+  (setq smudge-transport 'connect))
+
+(use-package espotify
+  :commands (espotify-next espotify-previous espotify-play-pause)
+  :straight t)
+
+;; * Misc
+
+(use-package pdf-tools
+  :straight t
+  :hook (pdf-view-mode . (lambda () (blink-cursor-mode -1)
+			   (setq-local cursor-type nil)))
+  :mode ("\\.pdf\\'" . pdf-view-mode)
+  :magic ("%PDF" . pdf-view-mode)
+  :config
+
+  (defun lg/pdf-tools-midnight-colors-with-theme (theme &rest args)
+    "Use the loaded theme background and foreground colors for the
+    midnight mode."
+    (setq pdf-view-midnight-colors
+	  `(,(face-foreground 'default) . ,(face-background 'default))))
+
+  (advice-add 'load-theme :after #'lg/pdf-tools-midnight-colors-with-theme))
+
+;; * Vterm
+(use-package vterm
+  ;; :ensure-system-package (libvterm . libvterm-dev)
+  :straight (vterm :type git :repo "akermu/emacs-libvterm")
+  :commands vterm
+  :init
+  (setq vterm-shell "bash")
+  (setq vterm-always-compile-module t)
+  (setq vterm-module-cmake-args "-DUSE_SYSTEM_VTERM=YES"))
+
+(use-package multi-vterm
+  :straight t
+  :after vterm)
+
+;; run-or-raise-or-dismiss for vterm
+(use-package vterm-toggle
+  :straight t
+  :after vterm
+  :bind ("s-<return>" . 'vterm-toggle)
+  :config
+  (setq vterm-toggle-fullscreen-p t))
+
+(use-package ibuffer
+  :commands ibuffer
+  :general (my-leader-def :keymaps 'override "bi" #'ibuffer)
+  :config (setq ibuffer-use-header-line nil))
+
+(use-package ibuffer-sidebar
+  :straight t
+  :commands ibuffer-sidebar-toggle-sidebar
+  :general
+  (my-leader-def :keymaps 'override
+    "tb" #'ibuffer-sidebar-toggle-sidebar)
+  :hook
+  (ibuffer-sidebar-mode . (lambda ()
+			    ;; avoid ibuffer hijacking the header-line
+			    (setq-local ibuffer-use-header-line nil)
+			    (setq-local ibuffer-header-line-format "Buffers")
+			    (setq header-line-format "Buffers")))
+  :config
+  (setq ibuffer-sidebar-width 30))
+  ;;(setq ibuffer-sidebar-mode-line-format nil))
+
+(use-package rg
+  :commands rg
+  :straight t
+  :ensure-system-package rg
+  :config (setq rg-executable "/usr/bin/rg"))
 
 (use-package magit
   :straight t
   :ensure-system-package git
+  :commands magit-status
   :general
   (my-leader-def
-    :prefix "SPC" :states '(normal emacs hybrid visual)
+    :keymaps 'override
+    "g" '(nil :which-key "Magit\n")
     "gg" #'magit-status))
+
+(use-package eww
+  :commands (eww eww-browse-with-history)
+  :general (my-leader-def :keymaps 'override "ow" #'eww-browse-with-history)
+  ;; :init
+  ;; (map! (:leader
+  ;;        :prefix ("o" . "open")
+  ;;        :desc "eww" "w" #'eww-browse-with-history)
+  ;;       (:map eww-link-keymap
+  ;;        ;"v" #'eww-mpv-video-at-point
+  ;;        "a" #'eww-mpv-audio-at-point
+  ;;        "C-j" #'eww-next-url
+  ;;        "C-k" #'eww-previous-url))
+  :config
+  (setq eww-download-directory "~/Téléchargements/eww/")
+  (setq eww-desktop-data-save '(:url :title))
+  (add-hook 'eww-after-render-hook #'prot-eww--rename-buffer)
+  (advice-add 'eww-back-url :after #'prot-eww--rename-buffer)
+  (advice-add 'eww-forward-url :after #'prot-eww--rename-buffer)
+  (use-package lg-eww
+    :load-path "lisp/site-packages"))
+
+(use-package bluetooth
+  :straight t
+  :commands bluetooth-list-devices)
+
+(use-package disk-usage
+  :straight t
+  :commands disk-usage)
+
+(use-package password-store
+  :straight t
+  :commands (password-store-copy password-store-insert)
+  :general
+  (my-leader-def
+    :keymaps 'override
+    "ip" '(password-store-copy :which-key "Copy password")))
+
+(use-package eshell-info-banner
+  :commands eshell
+  :straight (eshell-info-banner :type git
+                                :host github
+                                :repo "phundrak/eshell-info-banner.el")
+  :hook (eshell-banner-load . eshell-info-banner-update-banner))
 
 (provide 'lg-tools)
