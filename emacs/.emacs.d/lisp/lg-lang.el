68248754344683b3d7f9af5289cae0d3ea3ad24f
Lucas Gruss <lucas@debian>
2021-07-19 11:15:54 +0200

    emacs : add configuration for programming languages (lsp and python)

---
+++ emacs/.emacs.d/lisp/lg-lang.el
@@ -0,0 +1,23 @@
+;;; lg-lang.el : configuration for programming language
+
+;;; * LSP mode
+(use-package lsp-mode
+  :straight t)
+
+;;; * Python
+(use-package lsp-pyright
+  :ensure-system-package (npm nodejs (pyright . "npm install -g pyright"))
+  :straight t
+  :hook (python-mode . lsp))
+
+(use-package emacs
+  :config
+  (setq python-shell-interpreter "/home/lucas/.local/bin/ipython"))
+
+;;; * Formatting
+(use-package apheleia
+  :ensure-system-package (black . "pip3 install black")
+  :straight (apheleia :host github :repo "raxod502/apheleia")
+  :init
+  (add-to-list 'exec-path "/home/lucas/.local/bin/")
+  (apheleia-global-mode +1))
