6bb2975f5ed12a2eb1c87f80b5652f9ff9651082
Lucas Gruss <lucas@debian>
2022-01-03 13:06:38 +0100

    emacs: minor improvements to the style of init.el

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,5 +1,4 @@
-;; -*- lexical-binding: t; -*-
-;;; init.el : where all the magic starts
+;;; init.el : where all the magic starts -*- lexical-binding: t; -*-
 
 ;;; Add the modules to the load-path
 (defvar lg/configuration-path (expand-file-name "lisp" "~/.emacs.d"))
@@ -26,16 +25,6 @@
     )
   "List of enabled modules in my configuration.")
 
-(defun lg/require (package)
-  "Require a package, and print a message containing the load time
-on completion."
-  (let ((before-load-time (float-time)))
-    (require (intern package))
-    (message "Loaded %s in %s." package
-	     (format "%.2f seconds"
-		     (float-time
-		     (time-since before-load-time))))))
-
 (defun lg/emacs-startup-time-info ()
   "Profile emacs startup."
   (message "*** Emacs loaded in %s with %d garbage collections."
@@ -47,4 +36,10 @@ on completion."
 (add-hook 'emacs-startup-hook #'lg/emacs-startup-time-info)
 
 ;;; Require all the modules
-(mapcar #'lg/require lg/modules)
+(dolist (module lg/modules)
+  (let ((before-load-time (float-time)))
+    (require (intern module))
+    (message "Loaded %s in %s." module
+	     (format "%.2f seconds"
+		     (float-time
+		     (time-since before-load-time))))))
