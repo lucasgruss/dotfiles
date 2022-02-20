47fd01cea714230c2f34e30eda0735b0589fdbbe
Lucas Gruss <lucas@debian>
2022-02-20 22:21:54 +0100

    emacs: refine lg-lang module
    - LSP is still supported but the client is now eglot instead of lsp-mode.
    - DAP mode gets added and supports python
    - emacs-lisp-mode gets its own local transient

--- emacs/.emacs.d/lisp/lg-lang.el
+++ emacs/.emacs.d/lisp/lg-lang.el
@@ -1,22 +1,54 @@
 ;;; lg-lang.el --- configuration for programming language -*- lexical-binding: t; -*-
 
-;;; LSP mode
+;;; Tooling
+;;;; LSP mode
 (use-package lsp-mode
+  :disabled t
   :straight t
-  :after python-mode)
+  :unless (featurep 'eglot)
+  :defer t
+  :hook (python-mode . lsp)
+  :custom (read-process-output-max (* 1024 1024)) ;; 1mb
+  :init (setq lsp-keymap-prefix "s-x"))
+
+;;;; eglot
+(use-package eglot
+  :straight t
+  :defer t
+  :unless (featurep 'lsp-mode)
+  :hook (python-mode . eglot)
+  :custom
+  (read-process-output-max (* 1024 1024)) ;; 1mb
+  (eglot-autoreconnect 3))
+
+;;;; DAP : debugging
+(use-package dap-mode
+  :straight t
+  :defer t
+  :custom
+  (dap-auto-configure-features '(sessions locals controls tooltip)))
+
+;;;; DAP-python
+(use-package dap-python
+  :ensure-system-package (ptvsd . "pip install \"ptvsd>=4.2\"")
+  :straight nil
+  :after dap-mode)
 
 ;;; Python
+(use-package python
+  :defer t
+  :custom
+  (python-shell-interpreter "/usr/bin/python3"))
+
+;;;; LSP server
 (use-package lsp-pyright
   :ensure-system-package (npm nodejs (pyright . "npm install -g pyright"))
   :straight t
-  :hook (python-mode . lsp))
-
-(use-package emacs
-  :config
-  (setq python-shell-interpreter "/home/lucas/.local/bin/ipython"))
+  :after (:any eglot lsp))
 
 ;;; Formatting
 (use-package apheleia
+  :disabled t
   :straight (apheleia :host github :repo "raxod502/apheleia")
   :ensure-system-package ((black . "pip3 install black")
 			  (clang-format . "sudo npm install -g clang-format"))
@@ -26,6 +58,25 @@
   (add-to-list 'exec-path "/home/lucas/.local/bin/"))
 
 ;;; Emacs lisp
+(use-package emacs
+  :straight nil
+  :config
+  (when (featurep 'transient)
+    (define-transient-command lg/transient-elisp ()
+      "Emacs lisp mode"
+      [["Misc"
+	("e" "Eval" eval-buffer)
+	("n" "Narrow" outshine-narrow-to-subtree)
+	("N" "Narrow" narrow-to-defun)
+	("w" "Widen" widen)]]
+      [:hide (lambda () t)])
+
+    (general-define-key
+     :states 'normal
+     :keymaps 'emacs-lisp-mode-map
+     "<localleader>" 'lg/transient-elisp)))
+
+;;;; eldoc
 (use-package eldoc
   :straight nil
   :diminish (global-eldoc-mode eldoc-mode))
