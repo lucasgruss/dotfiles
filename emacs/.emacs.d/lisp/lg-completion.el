1b6ecafd81d24f067081d808243e3136b9602de5
Lucas Gruss <lucas@debian>
2022-02-20 22:17:21 +0100

    emacs: overhaul of the lg-completion module
    Company is swapped out for corfu. It looks to me that corfu feels snappier. That
    can also be because diving into in-buffer completion has helped me configure it
    more efficiently.

--- emacs/.emacs.d/lisp/lg-completion.el
+++ emacs/.emacs.d/lisp/lg-completion.el
@@ -1,8 +1,9 @@
 ;;; lg-completion --- completion configuration -*- lexical-binding: t; -*-
 ;; Author: Lucas Gruss
 
-;;;
 (use-package emacs
+  :custom
+  (tab-always-indent 'complete)
   :init
   (defun emacs-execute-extended-command ()
     "Create and select a frame called emacs-run-launcher which
@@ -99,7 +100,9 @@ behaviour. Delete the frame after that command has exited"
   :custom
   (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light))
   :config
-  (marginalia-mode +1))
+  (marginalia-mode +1)
+  (when (featurep 'all-the-icons-completion)
+    (add-hook 'marginalia-mode-hook #'all-the-icons-completion-marginalia-setup)))
 
 ;;; Consult
 (use-package consult
@@ -156,32 +159,121 @@ behaviour. Delete the frame after that command has exited"
   :after embark)
 
 ;;; Company
-(use-package company
-  :straight t
-  :diminish company-mode
-  :defer 10
-  :config (global-company-mode +1))
+;; (use-package company
+;;   :straight t
+;;   :diminish company-mode
+;;   :defer 10
+;;   :config (global-company-mode +1))
 
-;;; Company-box
-(use-package company-box
-  :diminish company-box-mode
-  :straight t
-  :after company
-  :hook (company-mode . company-box-mode))
+;; ;;; Company-box
+;; (use-package company-box
+;;   :diminish company-box-mode
+;;   :straight t
+;;   :after company
+;;   :hook (company-mode . company-box-mode))
 
-;;; Company-prescient
-(use-package company-prescient
-  :straight t
-  :after company
-  :config
-  (company-prescient-mode +1))
+;; ;;; Company-prescient
+;; (use-package company-prescient
+;;   :straight t
+;;   :after company
+;;   :config
+;;   (company-prescient-mode +1))
 
-;;; Company-ledger
-(use-package company-ledger
+;; ;;; Company-ledger
+;; (use-package company-ledger
+;;   :straight t
+;;   :disabled t
+;;   :after company
+;;   :config
+;;   (add-to-list 'company-backends 'company-ledger))
+
+;;; corfu
+(use-package corfu
   :straight t
+  :custom
+  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
+  (corfu-auto t)                 ;; Enable auto completion
+  (corfu-auto-prefix 4)                 ;; Enable auto completion
+  (corfu-auto-delay 0.3)                 ;; Enable auto completion
+  (corfu-separator ?\s)          ;; Orderless field separator
+  (corfu-quit-no-match 'separator)      ;; Never quit, even if there is no match
+  (corfu-preview-current t)    ;; Disable current candidate preview
+  (corfu-preselect-first nil)    ;; Disable candidate preselection
+  (corfu-on-exact-match nil)     ;; Configure handling of exact matches
+  (corfu-echo-documentation t) ;; Disable documentation in the echo area
+  (corfu-scroll-margin 5)        ;; Use scroll margin
+  :bind (:map corfu-map
+	      ("C-j" . corfu-next)
+	      ("C-k" . corfu-previous)
+	      ("C-l" . corfu-complete))
+  :init
+  (define-key evil-insert-state-map (kbd "C-k") nil)  ;; TODO remove once evil-collection supports corfu
+  (corfu-global-mode +1)
+  ;; :config
+  ;; (advice-add 'corfu--setup :after 'evil-normalize-keymaps)
+  ;; (advice-add 'corfu--teardown :after 'evil-normalize-keymaps)
+  ;; (evil-make-overriding-map corfu-map)
+  )
+
+;;;; corfu-doc
+(use-package corfu-doc
   :disabled t
-  :after company
-  :config
-  (add-to-list 'company-backends 'company-ledger))
+  :straight (:host github :type git :repo "galeo/corfu-doc")
+  :hook
+  (corfu-mode . corfu-doc-mode))
+
+;;;; cape
+(use-package cape
+  :straight t
+  ;; Bind dedicated completion commands
+  :bind (("C-c p p" . completion-at-point) ;; capf
+         ("C-c p t" . complete-tag)        ;; etags
+         ("C-c p d" . cape-dabbrev)        ;; or dabbrev-completion
+         ("C-c p f" . cape-file)
+         ("C-c p k" . cape-keyword)
+         ("C-c p s" . cape-symbol)
+         ("C-c p a" . cape-abbrev)
+         ("C-c p i" . cape-ispell)
+         ("C-c p l" . cape-line)
+         ("C-c p w" . cape-dict)
+         ("C-c p \\" . cape-tex)
+         ("C-c p _" . cape-tex)
+         ("C-c p ^" . cape-tex)
+         ("C-c p &" . cape-sgml)
+         ("C-c p r" . cape-rfc1345))
+  :init
+  ;; Add `completion-at-point-functions', used by `completion-at-point'.
+  (add-to-list 'completion-at-point-functions #'cape-file)
+  (add-to-list 'completion-at-point-functions #'cape-tex)
+  ;;(add-to-list 'completion-at-point-functions #'cape-dabbrev)
+  (add-to-list 'completion-at-point-functions #'cape-keyword)
+  ;;(add-to-list 'completion-at-point-functions #'cape-sgml)
+  ;;(add-to-list 'completion-at-point-functions #'cape-rfc1345)
+  ;;(add-to-list 'completion-at-point-functions #'cape-abbrev)
+  ;;(add-to-list 'completion-at-point-functions #'cape-ispell)
+  ;;(add-to-list 'completion-at-point-functions #'cape-dict)
+  ;;(add-to-list 'completion-at-point-functions #'cape-symbol)
+  ;;(add-to-list 'completion-at-point-functions #'cape-line))
+  )
+
+;;; yasnippets
+(use-package yasnippet
+  :straight t
+  :diminish yas-minor-mode
+  :hook
+  (org-mode . yas-minor-mode-on)
+  (prog-mode . yas-minor-mode-on))
+
+;;;; Yasnippet-snippets
+(use-package yasnippet-snippets
+  :straight t
+  :after yasnippet)
+
+;;;; Consult-yasnippet
+(use-package consult-yasnippet
+  :straight t
+  :after yasnippet
+  :bind (:map evil-normal-state-map
+	      ("gs" . consult-yasnippet)))
 
 (provide 'lg-completion)
